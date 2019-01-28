0cac57288a1c37e75bba90b5e5924843ec8e3c42
Patrick Monnerat <patrick@monnerat.net>
2019-01-28 03:52:24 +0100

    Upgrade submodule, sync sources.
    Gdb makefile does not provide -I for common subdirectory anymore.
    
    * gdbtk/generic/gdbtk.c: include common/version.h.

--- gdbtk/generic/gdbtk.c
+++ gdbtk/generic/gdbtk.c
@@ -29,7 +29,7 @@
 #include "gdbcore.h"
 #include "tracepoint.h"
 #include "demangle.h"
-#include "version.h"
+#include "common/version.h"
 #include "top.h"
 #include "annotate.h"
 #include "exceptions.h"
